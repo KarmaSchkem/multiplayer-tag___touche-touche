eac3a28c88927db5358ff146a39cc3eb3649d8e5
KarmaSchkem <151461062+KarmaSchkem@users.noreply.github.com>
2025-07-21 16:36:27 +0200

    Mise à jour pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,194 @@
- 
+namespace SpriteKind {
+    export const Player2 = SpriteKind.create()
+    export const Player3 = SpriteKind.create()
+    export const Player4 = SpriteKind.create()
+    export const Player1 = SpriteKind.create()
+}
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (mySprite.overlapsWith(mySprite2) && false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+})
+function _1 () {
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player1)
+    controller.player1.moveSprite(mySprite)
+    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
+}
+function _2 () {
+    mySprite2 = sprites.create(img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 6 f . . . . 
+        . . . f 1 6 6 6 6 6 1 6 f . . . 
+        . . . f 6 6 f f f f 6 1 f . . . 
+        . . . f 6 f f d d f f 6 f . . . 
+        . . f 6 f d f d d f d f 6 f . . 
+        . . f 6 f d 3 d d 3 d f 6 f . . 
+        . . f 6 6 f d d d d f 6 6 f . . 
+        . f 6 6 f 3 f f f f 3 f 6 6 f . 
+        . . f f d 3 5 3 3 5 3 d f f . . 
+        . . f d d f 3 5 5 3 f d d f . . 
+        . . . f f 3 3 3 3 3 3 f f . . . 
+        . . . f 3 3 5 3 3 5 3 3 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player2)
+    controller.player2.moveSprite(mySprite2)
+    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, mySprite2)
+}
+function ArrierePlan () {
+    splitScreen.setBorderColor(15)
+    if (Math.percentChance(25)) {
+        scene.setBackgroundColor(12)
+        tiles.setCurrentTilemap(tilemap`niveau2`)
+        Loup = statusbars.create(36, 4, StatusBarKind.Energy)
+        Loup.attachToSprite(mySprite, 7, 0)
+        Loup.setColor(2, 15)
+        Loup.setBarBorder(1, 15)
+    } else {
+        if (Math.percentChance(33)) {
+            scene.setBackgroundColor(12)
+            tiles.setCurrentTilemap(tilemap`niveau1`)
+            Loup = statusbars.create(36, 4, StatusBarKind.Energy)
+            Loup.attachToSprite(mySprite2, 7, 0)
+            Loup.setColor(2, 15)
+            Loup.setBarBorder(1, 15)
+        } else {
+            if (Math.percentChance(50)) {
+                scene.setBackgroundColor(12)
+                tiles.setCurrentTilemap(tilemap`niveau3`)
+                Loup = statusbars.create(36, 4, StatusBarKind.Energy)
+                Loup.attachToSprite(mySprite3, 7, 0)
+                Loup.setColor(2, 15)
+                Loup.setBarBorder(1, 15)
+            } else {
+                scene.setBackgroundColor(12)
+                tiles.setCurrentTilemap(tilemap`niveau4`)
+                Loup = statusbars.create(36, 4, StatusBarKind.Energy)
+                Loup.attachToSprite(mySprite4, 7, 0)
+                Loup.setColor(2, 15)
+                Loup.setBarBorder(1, 15)
+            }
+        }
+    }
+}
+function _4 () {
+    mySprite4 = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f 1 1 1 1 f f . . . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . . f 1 1 1 1 1 1 1 1 f . . . 
+        . . f d 1 1 1 1 1 1 1 1 d f . . 
+        . . f d 1 1 1 1 1 1 1 1 d f . . 
+        . . f d d d 1 1 1 1 d d d f . . 
+        . . f b d b f d d f b d b f . . 
+        . . f c d c f 1 1 f c d c f . . 
+        . . . f b 1 1 1 1 1 1 b f . . . 
+        . . f f f c d b 1 b d f f f f . 
+        f c 1 1 1 c b f b f c 1 1 1 c f 
+        f 1 b 1 b 1 f f f f 1 b 1 b 1 f 
+        f b f b f f f f f f b f b f b f 
+        . . . . . f f f f f f . . . . . 
+        . . . . . . . f f f . . . . . . 
+        `, SpriteKind.Player4)
+    controller.player4.moveSprite(mySprite4)
+    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera4, mySprite4)
+}
+function StatusBars () {
+    statusbar = statusbars.create(36, 4, StatusBarKind.Health)
+    statusbar.attachToSprite(mySprite, 2, 0)
+    statusbar.max = 3
+    statusbar.setBarBorder(1, 15)
+}
+function Boot () {
+    Personnage()
+    ArrierePlan()
+    statusbars_game()
+}
+function Personnage () {
+    _1()
+    _2()
+    _3()
+    _4()
+}
+function StatusBars3 () {
+    statusbar3 = statusbars.create(36, 4, StatusBarKind.Health)
+    statusbar3.attachToSprite(mySprite3, 2, 0)
+    statusbar3.max = 3
+    statusbar3.setBarBorder(1, 15)
+}
+function statusbars_game () {
+    StatusBars()
+    StatusBars2()
+    StatusBars3()
+    StatusBars4()
+}
+function StatusBars2 () {
+    statusbar2 = statusbars.create(36, 4, StatusBarKind.Health)
+    statusbar2.attachToSprite(mySprite2, 2, 0)
+    statusbar2.max = 3
+    statusbar2.setBarBorder(1, 15)
+}
+function StatusBars4 () {
+    statusbar4 = statusbars.create(36, 4, StatusBarKind.Health)
+    statusbar4.attachToSprite(mySprite4, 2, 0)
+    statusbar4.max = 3
+    statusbar4.setBarBorder(1, 15)
+}
+function _3 () {
+    mySprite3 = sprites.create(img`
+        . . . . . f f 4 4 f f . . . . . 
+        . . . . f 5 4 5 5 4 5 f . . . . 
+        . . . f e 4 5 5 5 5 4 e f . . . 
+        . . f b 3 e 4 4 4 4 e 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e b 3 e e 3 b e 3 3 f . 
+        . f 3 3 f f e e e e f f 3 3 f . 
+        . f b b f b f e e f b f b b f . 
+        . f b b e 1 f 4 4 f 1 e b b f . 
+        f f b b f 4 4 4 4 4 4 f b b f f 
+        f b b f f f e e e e f f f b b f 
+        . f e e f b d d d d b f e e f . 
+        . . e 4 c d d d d d d c 4 e . . 
+        . . e f b d b d b d b b f e . . 
+        . . . f f 1 d 1 d 1 d f f . . . 
+        . . . . . f f b b f f . . . . . 
+        `, SpriteKind.Player3)
+    controller.player3.moveSprite(mySprite3)
+    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera3, mySprite3)
+}
+let statusbar4: StatusBarSprite = null
+let statusbar2: StatusBarSprite = null
+let statusbar3: StatusBarSprite = null
+let statusbar: StatusBarSprite = null
+let mySprite4: Sprite = null
+let mySprite3: Sprite = null
+let Loup: StatusBarSprite = null
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 320
+    export const ARCADE_SCREEN_HEIGHT = 240
+}
+Boot()
